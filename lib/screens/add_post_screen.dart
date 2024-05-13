063f11462109ce919b37780e5559864c9947ddbf
naufal192 <naufalalmubarak192@mhs.mdp.ac.id>
2024-05-13 15:04:38 +0700

    Post
    Post

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AddPostScreen extends StatefulWidget {
   @override
@@ -64,8 +65,33 @@ class _AddPostScreenState extends State<AddPostScreen> {
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                // Add logic to save post to Firestore here
-                Navigator.pop(context);
+                // Cek apakah ada teks post dan gambar telah dipilih
+                if (_postTextController.text.isNotEmpty && _imageUrl != null) {
+                  // Menyimpan pos ke Firestore
+                  FirebaseFirestore.instance.collection('posts').add({
+                    'text': _postTextController.text,
+                    'image_url': _imageUrl,
+                    'timestamp': Timestamp.now(),
+                  }).then((_) {
+                    // Jika penyimpanan berhasil, kembali ke layar sebelumnya
+                    Navigator.pop(context);
+                  }).catchError((error) {
+                    // Jika terjadi kesalahan, tampilkan pesan error
+                    print('Error saving post: $error');
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Failed to save post. Please try again.'),
+                      ),
+                    );
+                  });
+                } else {
+                  // Jika teks post atau gambar tidak tersedia, tampilkan pesan
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Please write a post and select an image.'),
+                    ),
+                  );
+                }
               },
               child: Text('Post'),
             ),
